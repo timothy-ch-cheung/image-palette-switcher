263217dd121e3b070afe92751936a7b65b7cbc32
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2023-10-22 01:27:59 +0100

    Add dropzone with error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import DropzoneBox from "./components/DropzoneBox";
+import { AlertProvider } from "./components/AlertContext";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <AlertProvider>
+        <div className="App">
+          <DropzoneBox />
+        </div>
+      </AlertProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/AlertContext.tsx
@@ -0,0 +1,50 @@
+import { Alert, Box, Collapse } from "@mui/material";
+import React, { createContext, useContext, useState } from "react";
+
+type AlertContextType = {
+  isVisible: boolean;
+  message: string;
+  showAlert: (message: string) => void;
+};
+
+const AlertContext = createContext<AlertContextType>({
+    isVisible: false,
+    message: "",
+    showAlert: () => {}
+});
+
+export function useAlert() {
+  return useContext(AlertContext);
+}
+
+interface AlertProviderProps {
+  children: React.ReactNode;
+}
+
+export function AlertProvider(props: AlertProviderProps) {
+  const [isVisible, setIsVisible] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const showAlert = (message: string) => {
+    setMessage(message);
+    setIsVisible(true);
+    setTimeout(() => setIsVisible(false), 2000);
+  };
+
+  const value: AlertContextType = {
+    isVisible,
+    message,
+    showAlert,
+  };
+
+  return (
+    <AlertContext.Provider value={value}>
+        <Collapse in={isVisible}>
+          <Alert severity="error" style={{ position: "absolute", width: "75vw", left: "12.5%", top: "2.5%"}}>
+            {message}
+          </Alert>
+        </Collapse>
+      {props.children}
+    </AlertContext.Provider>
+  );
+}
